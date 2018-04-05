a0ecefc62f091ea5cf38d5f55edae881dabd38c2
pedro.sousa <pedro.sousa@thought-creator.com>
2018-04-05 09:42:37 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import './style.sass';
+// import './style.sass';
 
 const App = () => (
     <div>
-    <h1>Hello, world!</h1>
-</div>
+        <h1>Hello, world!</h1>
+        <h2>Se aparecer uma imagem em baixo é porque o Nuno 'El Coxo' é cona.</h2>
+        <img src={"http://www.gazetadopovo.com.br/agronegocio/ra/media/Pub/GP/p4/2017/11/13/Agronegocio/Imagens/Vivo/Wagyu.jpg"}/>
+    </div>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
